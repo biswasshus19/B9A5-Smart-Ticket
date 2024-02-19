0df395e9c4bbb85b5ee4d3fd35555827366a1de4
biswasshus19 <shusmitatonu6@gmail.com>
2024-02-19 18:11:39 +0600

    hi this is a dinamic bus tickets section

---
+++ utility.js
@@ -0,0 +1,15 @@
+function setElementValueById(elementId, value) {
+    document.getElementById(elementId).innerText = value;
+  }
+  function totalCost(id, value) {
+    const totalCost = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const sum = convertedTotalCost + parseInt(value);
+    setElementValueById(id, sum);
+  }
+  
+  function getInputValueById(elementId) {
+    const input = document.getElementById(elementId);
+    const inputValue = input.value;
+    return inputValue;
+  }
